9492a0a103c513851ef3993d2fce982951a59e88
Elivelton <elivelton1329@gmail.com>
2021-11-10 23:53:45 -0300

    Implementação de eventos nos campos de input

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const valorInput = document.getElementById('valorDaConta')
 const pessoasInput = document.getElementById('numeroDePessoas')
 const outraPorcentagemInput = document.getElementById('outraPorcentagem')
 
-let warningValor = 0
+let warningValor = false
+let warningGorjeta = false
+let warningPessoas = false
 
 
 valorInput.addEventListener("keypress", function (e) {
@@ -12,19 +14,21 @@ valorInput.addEventListener("keypress", function (e) {
     }
 
     setTimeout(() => {
-        if (parseInt(valorInput.value) == 0 && warningValor == 0) {
+        if (parseFloat(valorInput.value) == 0 && !warningValor) {
         const novoEl = document.createElement('h3')
         const novoP = document.createTextNode('O valor não pode ser 0')
+        valorInput.classList.add('inputWarning')
         novoEl.appendChild(novoP)
         const posicao = document.getElementById('wrap')
         novoEl.classList.add('warningValor')
         posicao.appendChild(novoEl)
-        warningValor = 1
-    } else if (parseInt(valorInput.value) != 0 && warningValor == 1) {
+        warningValor = true
+    } else if (parseFloat(valorInput.value) != 0 && warningValor) {
         const removerEl = document.getElementsByClassName('warningValor')[0]
         const container = removerEl.parentNode
         container.removeChild(removerEl)
-        warningValor = 0
+        warningValor = false
+        valorInput.classList.remove('inputWarning')
     }
 
     }, 100);
@@ -41,10 +45,28 @@ pessoasInput.addEventListener("keypress", function (e) {
         e.preventDefault();
     }
 
-    if (pessoasInput.value.length > 100 || pessoasInput.value.length <= 0) {
-        e.preventDefault()
-    } else if (keyCode == 48) {
+    setTimeout(() => {
+        if (parseFloat(pessoasInput.value) == 0 && !warningPessoas) {
+        const novoEl = document.createElement('h3')
+        const novoP = document.createTextNode('O valor não pode ser 0')
+        novoEl.appendChild(novoP)
+        const posicao = document.getElementById('wrapPessoas')
+        novoEl.classList.add('warningValor')
+        posicao.appendChild(novoEl)
+        pessoasInput.classList.add('inputWarning')
+        warningPessoas = true
+    } else if (parseFloat(valorInput.value) != 0 && warningPessoas) {
+        const removerEl = document.getElementsByClassName('warningValor')[0]
+        const container = removerEl.parentNode
+        container.removeChild(removerEl)
+        warningPessoas = false
+        pessoasInput.classList.remove('inputWarning')
+    }
 
+    }, 100);
+
+    if (pessoasInput.value.length >= 2){
+        e.preventDefault()
     }
 })
 
@@ -52,12 +74,29 @@ outraPorcentagemInput.addEventListener("keypress", function (e) {
     let keyCode = (e.keyCode ? e.keyCode : e.which);
     if (keyCode == 45) {
         e.preventDefault();
-    } else if (keyCode == 48) {
-
     }
 
-    if (outraPorcentagemInput.value.length >= 4 || outraPorcentagemInput.value.length <= 0) {
+    setTimeout(() => {
+        if (parseFloat(outraPorcentagemInput.value) == 0 && !warningGorjeta) {
+        const novoEl = document.createElement('h3')
+        const novoP = document.createTextNode('O valor não pode ser 0')
+        outraPorcentagemInput.classList.add('inputWarning')
+        novoEl.appendChild(novoP)
+        const posicao = document.getElementById('gorjetas')
+        novoEl.classList.add('warningValorPorcentagem')
+        posicao.appendChild(novoEl)
+        warningGorjeta = 1
+    } else if (parseFloat(valorInput.value) != 0 && warningGorjeta == 1) {
+        outraPorcentagemInput.classList.remove('inputWarning')
+        const removerEl = document.getElementsByClassName('warningValorPorcentagem')[0]
+        const container = removerEl.parentNode
+        container.removeChild(removerEl)
+        warningGorjeta = 0
+    }
+
+    }, 100);
+
+    if (outraPorcentagemInput.value.length >= 2) {
         e.preventDefault()
-    } else if (keyCode == 48) {
     }
 })
