98f9271add8d4b421f67394a3d4e3059c0ed1e69
Elivelton <elivelton1329@gmail.com>
2021-11-13 18:39:27 -0300

    Refatoração do código Js; menos linhas e mais eficiência.

--- script.js
+++ script.js
@@ -1,265 +1,72 @@
+const spanInputValor = document.querySelector('.warningSpan')
+const spanInputPessoas = document.querySelector('.warningSpanPessoas')
+const botoes = document.getElementById('gorjetas')
 const valorInput = document.getElementById('valorDaConta')
 const pessoasInput = document.getElementById('numeroDePessoas')
-const outraPorcentagemInput = document.getElementById('outraPorcentagem')
-
-const botao1 = document.querySelector('[botao1]')
-const botao2 = document.querySelector('[botao2]')
-const botao3 = document.querySelector('[botao3]')
-const botao4 = document.querySelector('[botao4]')
-const botao5 = document.querySelector('[botao5]')
-
-const listaBotao = [botao1, botao2, botao3, botao4, botao5]
 
 
-let botaoAtivo = false
-
-
-let warningValor = false
-let warningGorjeta = false
-let warningPessoas = false
-
-let valorDaGorjeta
-let valorDaPessoa
-let campoInputValor = false
-let campoInputPessoas = false
-
-
-valorInput.addEventListener("keypress", function (e) {
-    let keyCode = (e.keyCode ? e.keyCode : e.which)
-    if (keyCode == 45) {
-        e.preventDefault();
-    }
-
-    setTimeout(() => {
-        if (parseFloat(valorInput.value) == 0 && !warningValor) {
-            adicionaWarningValor()
-        } else if (parseFloat(valorInput.value) != 0 && warningValor) {
-            removeWarningValor()
-        }
-
-    }, 100);
-
-
-    if (valorInput.value.length >= 6) {
-        e.preventDefault()
+valorInput.addEventListener('keyup', function () {
+    if (parseInt(valorInput.value) == 0 || valorInput.value == '') {
+        adicionaWarningValor()
+    } else {
+        removeWarningValor()
     }
 })
 
-pessoasInput.addEventListener("keypress", function (e) {
-    let keyCode = (e.keyCode ? e.keyCode : e.which);
-    if (keyCode == 45) {
-        e.preventDefault();
-    }
-
-    setTimeout(() => {
-        if (parseFloat(pessoasInput.value) == 0 && !warningPessoas) {
-            adicionaWarningPessoas()
-        } else if (parseFloat(valorInput.value) != 0 && warningPessoas) {
-            removeWarningPessoas()
-        }
-
-    }, 100);
-
-    if (pessoasInput.value.length >= 5) {
-        e.preventDefault()
+pessoasInput.addEventListener('keyup', function () {
+    if (parseInt(pessoasInput.value) == 0 || pessoasInput.value == '') {
+        adicionaWarningPessoas()
+    } else {
+        removeWarningPessoas()
     }
 })
-
-outraPorcentagemInput.addEventListener("keypress", function (e) {
-    let keyCode = (e.keyCode ? e.keyCode : e.which);
-    if (keyCode == 45) {
-        e.preventDefault();
-    }
-
-    setTimeout(() => {
-        if (parseFloat(outraPorcentagemInput.value) == 0 && !warningGorjeta) {
-            const novoEl = document.createElement('h3')
-            const novoP = document.createTextNode('O valor não pode ser 0')
-            outraPorcentagemInput.classList.add('inputWarning')
-            novoEl.appendChild(novoP)
-            const posicao = document.getElementById('gorjetas')
-            novoEl.classList.add('warningValorPorcentagem')
-            posicao.appendChild(novoEl)
-            warningGorjeta = 1
-        } else if (parseFloat(valorInput.value) != 0 && warningGorjeta == 1) {
-            outraPorcentagemInput.classList.remove('inputWarning')
-            const removerEl = document.getElementsByClassName('warningValorPorcentagem')[0]
-            const container = removerEl.parentNode
-            container.removeChild(removerEl)
-            warningGorjeta = 0
-        }
-
-    }, 100);
-
-    if (outraPorcentagemInput.value.length >= 3) {
-        e.preventDefault()
-    }
-})
-
-function checaBotao() {
-    for (let i = 0; i < listaBotao.length; i++) {
-        if (listaBotao[i].hasAttribute('class', 'botaoClicado')) {
-            botaoAtivo = true
-        }
-    }
-}
-
-function removeClasseBotao() {
-    for (let i = 0; i < listaBotao.length; i++) {
-        if (listaBotao[i].hasAttribute('class', 'botaoClicado')) {
-            listaBotao[i].removeAttribute('class', 'botaoClicado')
-            botaoAtivo = false
-        }
-    }
-}
-
-function checaInputs() {
-    if (valorInput.value != '' &&  valorInput.value != '0' && pessoasInput.value != '' && pessoasInput.value != '0') {
-        console.log('entrei no if 1')
-        campoInputValor = true
-        campoInputPessoas = true
-        return inputsChecados = true
-    } else if (valorInput.value != '' &&  valorInput.value != '0' && pessoasInput.value == '' && pessoasInput.value != '0' ) {
-        console.log('entrei no if 2')
-        campoInputPessoas = false
-        campoInputValor = true
-        if (warningPessoas == false) {
-            adicionaWarningPessoas()
-        }
-        return inputsChecados = false
-    } else if (pessoasInput.value != '' && pessoasInput.value != '0' && valorInput.value == '' && valorInput.value != '0') {
-        console.log('entrei no if 3')
-        campoInputPessoas = true
-        campoInputValor = false
-        if (warningValor == false) {
-            adicionaWarningValor()
-        }
-        return inputsChecados = false
-    } else if  (pessoasInput.value == '0' || pessoasInput.value == '' && valorInput.value == '0' || valorInput.value == '') {
-        if (warningPessoas == false ) {
-            adicionaWarningPessoas()
-        }
-
-        if (warningValor == false) {
-            adicionaWarningValor()
-        }
-    }
-}
 
 function adicionaWarningPessoas() {
-    const novoEl = document.createElement('h3')
-    const novoP = document.createTextNode('O valor não pode ser 0')
-    novoEl.appendChild(novoP)
-    const posicao = document.getElementById('wrapPessoas')
-    novoEl.classList.add('warningValor')
-    posicao.appendChild(novoEl)
+    spanInputPessoas.setAttribute('style', 'display: inline;')
     pessoasInput.classList.add('inputWarning')
-    warningPessoas = true
 }
 
 function removeWarningPessoas() {
-    const removerEl = document.getElementsByClassName('warningValor')[0]
-    const container = removerEl.parentNode
-    container.removeChild(removerEl)
-    warningPessoas = false
+    spanInputPessoas.setAttribute('style', 'display: none;')
     pessoasInput.classList.remove('inputWarning')
 }
 
 function adicionaWarningValor() {
-    const novoEl = document.createElement('h3')
-    const novoP = document.createTextNode('O valor não pode ser 0')
+    spanInputValor.setAttribute('style', 'display: inline;')
     valorInput.classList.add('inputWarning')
-    novoEl.appendChild(novoP)
-    const posicao = document.getElementById('wrap')
-    novoEl.classList.add('warningValor')
-    posicao.appendChild(novoEl)
-    warningValor = true
 }
 
 function removeWarningValor() {
-    const removerEl = document.getElementsByClassName('warningValor')[0]
-    const container = removerEl.parentNode
-    container.removeChild(removerEl)
-    warningValor = false
+    spanInputValor.setAttribute('style', 'display: none;')
     valorInput.classList.remove('inputWarning')
 }
 
-
-botao1.addEventListener('click', function (e) {
-    checaInputs()
-    if (campoInputPessoas && campoInputValor) {
-        checaBotao()
-        if (botaoAtivo == false) {
-            e.target.classList.add('botaoClicado')
-        } else {
-            removeClasseBotao()
-            e.target.classList.add('botaoClicado')
+function removeClasseBotao() {
+    let filhos = botoes.childNodes
+    filhos.forEach(function(item, indice) {
+        if (indice % 2 != 0) {
+            if (item.hasAttribute('class', 'botaoClicado')) {
+                item.removeAttribute('class', 'botaoClicado')
+            }
         }
-    } else {
-        checaInputs()
-    }
-})
+    })
+}
 
-botao2.addEventListener('click', function (e) {
-    checaInputs()
-    if (campoInputPessoas && campoInputValor) {
-        checaBotao()
-        
-        if (botaoAtivo == false) {
-            e.target.classList.add('botaoClicado')
-        } else {
-            removeClasseBotao()
-            e.target.classList.add('botaoClicado')
-        }
-    } else {
-        checaInputs()
-    }
-})
+botoes.addEventListener('click', function (e) {
+    const inputPessoasChecado = (spanInputPessoas.getAttribute('style') == 'display: none;')
+    const inputValorChecado = (spanInputValor.getAttribute('style') == 'display: none;')
+    botaoDisparado = e.target
 
-botao3.addEventListener('click', function (e) {
-    checaInputs()
-    if (campoInputPessoas && campoInputValor) {
-        checaBotao()
-        
-        if (botaoAtivo == false) {
-            e.target.classList.add('botaoClicado')
-        } else {
-            removeClasseBotao()
-            e.target.classList.add('botaoClicado')
-        }
+    if(botaoDisparado.tagName == 'BUTTON' && inputValorChecado && inputPessoasChecado) {
+        removeClasseBotao()
+        botaoDisparado.classList.add('botaoClicado')
+        removeWarningPessoas()
+        removeWarningValor()
     } else {
-        checaInputs()
-    }
-})
-
-botao4.addEventListener('click', function (e) {
-    checaInputs()
-    if (campoInputPessoas && campoInputValor) {
-        checaBotao()
-        
-        if (botaoAtivo == false) {
-            e.target.classList.add('botaoClicado')
-        } else {
-            removeClasseBotao()
-            e.target.classList.add('botaoClicado')
+        if ((parseInt(valorInput.value) == 0 || valorInput.value == '')) {
+            adicionaWarningValor()
+        } else if (parseInt(pessoasInput.value) == 0 || pessoasInput.value == '') {
+            adicionaWarningPessoas()
         }
-    } else {
-        checaInputs()
-    }
-})
-
-botao5.addEventListener('click', function (e) {
-    checaInputs()
-    if (campoInputPessoas && campoInputValor) {
-        checaBotao()
-        
-        if (botaoAtivo == false) {
-            e.target.classList.add('botaoClicado')
-        } else {
-            removeClasseBotao()
-            e.target.classList.add('botaoClicado')
-        }
-    } else {
-        checaInputs()
     }
 })
